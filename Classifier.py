fa96379d6964ee714f7d442907959a051748db0d
willcastl3 <willcastl3@outlook.com>
2024-02-29 02:07:07 -0600

    Implemented dataset population from MongoDB collections and main function for data retrieval

--- Classifier.py
+++ Classifier.py
@@ -1,5 +1,6 @@
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
+from mongodb_connection import get_database
 
 
 def process_text(text):
@@ -14,3 +15,37 @@ def process_text(text):
     tagged_tokens = pos_tag(tokens)
     keywords_set = {token for token, tag in tagged_tokens if tag.startswith('N') or tag.startswith('J')}
     return ' '.join(keywords_set)
+
+
+def populate_dataset(dbname, movieBank, dataset):
+    """
+    Populates the dataset with keywords extracted from the plot summaries
+    stored in MongoDB collections corresponding to different movie genres.
+
+    :param dbname: The MongoDB database object.
+    :param movieBank: A list of strings representing the names of collections in the database.
+    :param dataset: The dataset to be populated with keyword-genre pairs.
+    """
+    for genres in movieBank:
+        collection_name = dbname[genres]
+        item_details = collection_name.find()
+
+        for item in item_details:
+            keywords = process_text(item["plot"])
+            dataset.append((keywords, item["genre"]))
+
+
+def main():
+    dbname = get_database()
+    movieBank = ["Top_100_Action", "Top_100_Adventure", "Top_100_Comedies", "Top_100_Drama", "Top_100_Fantasy",
+                 "Top_100_Horror", "Top_100_Mystery", "Top_100_Romantic", "Top_100_Sci-Fi", "Top_100_Thrillers",
+                 "Best_Raunchy_Comedies", "Best_2000s_Comedy"]
+    dataset = []
+
+    populate_dataset(dbname, movieBank, dataset)
+
+    print(dataset)
+
+
+if __name__ == "__main__":
+    main()

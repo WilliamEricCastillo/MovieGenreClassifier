5c2638496a370108a1627b2631c8a0f4c61e3d35
willcastl3 <willcastl3@outlook.com>
2024-02-29 01:57:05 -0600

    updated doc string

--- Classifier.py
+++ Classifier.py
@@ -3,6 +3,13 @@ from nltk import pos_tag
 
 
 def process_text(text):
+    """
+    Tokenizes the input text, performs part-of-speech tagging,
+    and extracts adjectives and nouns to generate keywords.
+
+    :param text: The input text to be processed.
+    :return: A string containing the extracted keywords.
+    """
     tokens = word_tokenize(text)
     tagged_tokens = pos_tag(tokens)
     keywords_set = {token for token, tag in tagged_tokens if tag.startswith('N') or tag.startswith('J')}
